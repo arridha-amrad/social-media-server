5ee926c226ad87bff1fce8708b379575f5a13189
Arridha <arridhaamrad@gmail.com>
2022-01-31 15:39:52 +0700

    add request limiter for post routes

--- notes/docker.md
+++ notes/docker.md
@@ -1,2 +1,10 @@
 docker compose --env-file .env.development config
 docker-compose --env-file .env.development up
+
+**Redis**
+
+- run redis with via docker compose
+- docker exec -it <container_name> sh
+- redis-cli
+- get all keys -> keys '\*'
+- find keys with pattern -> keys \*\_refToken
--- src/routes/PostRoutes.ts
+++ src/routes/PostRoutes.ts
@@ -8,15 +8,35 @@ import {
   likeDislikeHandler,
 } from '../controllers/PostController';
 import { verifyAccessToken } from '../services/JwtService';
+import rateLimit from 'express-rate-limit';
+
+const apiLimiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 10, // Limit each IP to 10 requests per `window` (here, per 1 minute)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+const apiLimiterCreatePost = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 2, // Limit each IP to 10 requests per `window` (here, per 1 minute)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
 
 // eslint-disable-next-line
 const router = express.Router();
 
-router.get('/', verifyAccessToken, getPostsHandler);
+router.get('/', apiLimiter, verifyAccessToken, getPostsHandler);
 router.get('/:id', verifyAccessToken, getPostHandler);
-router.post('/', verifyAccessToken, createPostHandler);
+router.post('/', apiLimiterCreatePost, verifyAccessToken, createPostHandler);
 router.put('/:id', verifyAccessToken, updatePostHandler);
 router.delete('/:id', verifyAccessToken, deletePostHandler);
-router.post('/like-dislike/:id', verifyAccessToken, likeDislikeHandler);
+router.post(
+  '/like-dislike/:id',
+  apiLimiter,
+  verifyAccessToken,
+  likeDislikeHandler
+);
 
 export default router;
